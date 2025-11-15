f206e881ab026f46ce75ec38bc977293dbe4f7c3
Diksha <diksha@macbookpro>
2025-11-14 17:05:07 -0800

    starting work

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// COMSC-210 | Lab 32 | Diksha Tara Natesan
+// IDE used: Vim/Terminal
+
+#include "Car.h"
+#include <deque>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+
+
+
+}
+
