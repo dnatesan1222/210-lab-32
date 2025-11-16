6c283253e239588adfb90cacf28656d40b9fa897
Diksha <diksha@macbookpro>
2025-11-15 18:46:07 -0800

    working on loop logic

--- main.cpp
+++ main.cpp
@@ -19,55 +19,58 @@ int main(){
 	srand(time(0));
 	array<deque<Car>, 4> plaza;
 	/*cout << "Empty lanes:\n";	//part of milestone 3 testing
-	displayQueue(plaza);*/
+	  displayQueue(plaza);*/
 	for (int i = 0; i < 4; i++){
 		Car c1;
 		Car c2;
 		plaza[i].push_back(c1);
 		plaza[i].push_back(c2);
 		/*//Milestone 3 tests
-		cout << "Lane " << (i+1) << " populated\n";
-		displayQueue(plaza);*/
+		  cout << "Lane " << (i+1) << " populated\n";
+		  displayQueue(plaza);*/
 	}
-	
+
 	cout << "Initial queues:" << endl;
 	displayQueue(plaza);
 
 	/*//Milestone 2 tests
-	cout << "Tests:\nFirst car in lane 1 queue: ";
-	plaza[0].front().print();
-	cout << "Last car in lane 4 queue: ";
-        plaza[3].back().print();
-	cout << "Removing Lane 3's first car:" << endl;
-	Car temp = plaza[2].front();
-	plaza[2].pop_front();
-	displayQueue(plaza[2]);
-	cout << "Adding Lane 3's first car back:" << endl;
-	plaza[2].push_front(temp);
-	displayQueue(plaza[2]);*/
-/*
+	  cout << "Tests:\nFirst car in lane 1 queue: ";
+	  plaza[0].front().print();
+	  cout << "Last car in lane 4 queue: ";
+	  plaza[3].back().print();
+	  cout << "Removing Lane 3's first car:" << endl;
+	  Car temp = plaza[2].front();
+	  plaza[2].pop_front();
+	  displayQueue(plaza[2]);
+	  cout << "Adding Lane 3's first car back:" << endl;
+	  plaza[2].push_front(temp);
+	  displayQueue(plaza[2]);*/
+
 	int prob;
 	int time = 1;
 	//start loop here for simulation
-	while (tollQueue.size() != 0){
-		cout << "Time: " << time << " Operation: ";
-		prob = rand() % 100 + 1;  	//returns random number 1-100 for probability functions
-		if (prob < 56){			//55% probability
-			cout << "Car paid: ";
-			(tollQueue.front()).print();
-			tollQueue.pop_front();
-		}else{				//45% probability
-			Car temp;
-			tollQueue.push_back(temp);
-			cout << "Joined lane: ";
-			temp.print();
+	for (int i = 1; i < 21; i++){
+		cout << "Time: " << time << endl;
+		for (int j = 1; j < 5; j++){
+			cout << "Lane " << j;
+			prob = rand() % 100 + 1;  	//returns random number 1-100 for probability functions
+			if (prob < 51){			//50% probability
+				cout << " Paid: ";
+				(plaza[i].front()).print();
+				plaza[i].pop_front();
+			}else{				//40% probability
+				Car temp;
+				plaza[i].push_back(temp);
+				cout << " Joined: ";
+				temp.print();
+			}
+			cout << endl;
 		}
-		cout << "Queue: " << endl;
-		displayQueue(tollQueue);
+		displayQueue(plaza);
 		time += 1;
 	}
 	cout << endl;
-*/	return 1;
+	return 1;
 }
 
 void displayQueue(deque<Car> &tQ){
@@ -83,15 +86,15 @@ void displayQueue(deque<Car> &tQ){
 	}
 }
 void displayQueue(array<deque<Car>, 4> &tQ){
-        //this will not output "Queue:" or "Initial queue:" that will happen before the function is called
-        if (tQ.size() == 0){
-                cout << "\tEmpty" << endl;
-                return;
-        }
-        for (int i = 0; i < 4; i++){
+	//this will not output "Queue:" or "Initial queue:" that will happen before the function is called
+	if (tQ.size() == 0){
+		cout << "\tEmpty" << endl;
+		return;
+	}
+	for (int i = 0; i < 4; i++){
 		cout << "Lane " << (i+1) << " Queue:\n";
 		displayQueue(tQ[i]);
-        }
-        cout << endl;
+	}
+	cout << endl;
 }
 
